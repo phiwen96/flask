8588bc91b230dca16893355798d770c2f765ac64
Philip Wenkel <38218064+phiwen96@users.noreply.github.com>
2021-05-19 17:37:39 +0200

    a

--- xbuild/src/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/philipwenkel/Github/flask/src
-# Build directory: /Users/philipwenkel/Github/flask/xbuild/src
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
